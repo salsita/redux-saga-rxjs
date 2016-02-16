1bf221da923af36f87f2f29ecbad1d334dfd9ecd
Tomas Weiss <tomasw@salsitasoft.com>
2016-02-16 11:22:13 +0100

    couple fundamental tests, passing state to iterable chain

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import { Subject } from 'rxjs';
 
+const isFunction = any => typeof any === 'function';
+
+const invariant = (condition, message) => {
+  if (condition) {
+    throw new Error(`Invariant violation: ${message}`);
+  }
+};
+
 export default (...sagas) => {
   const subject = new Subject();
 
+  invariant(!sagas.every(isFunction),
+      'All the provided sagas must be typeof function');
+
   return store => {
     sagas.forEach(saga =>
-        saga(subject).subscribe(dispatchable => store.dispatch(dispatchable)));
+        saga(subject)
+          .subscribe(dispatchable => store.dispatch(dispatchable)));
 
     return next => action => {
       next(action);
-      subject.next(action, store.getState());
+      subject.next({action, state: store.getState()});
     };
   };
 };
---
+++ test/sagaMiddleware.test.js
@@ -0,0 +1,34 @@
+import { spy } from 'sinon';
+import { assert } from 'chai';
+
+import sagaMiddleware from '../src/index';
+import { createStore, applyMiddleware } from 'redux';
+
+describe('SagaMiddleware test', () => {
+  it('should allow mounting multiple sagas which may potentially interact in action chain', () => {
+    const sagaA = iterable => iterable
+      .filter(({ action }) => action.type === 'FOO')
+      .map(() => ({type: 'BAR'}));
+
+    const sagaB = iterable => iterable
+      .filter(({ action }) => action.type === 'BAR')
+      .map(() => ({type: 'BAZ'}));
+
+    const reducer = spy((appState = 0) => appState);
+    const store = applyMiddleware(sagaMiddleware(sagaA, sagaB))(createStore)(reducer);
+    store.dispatch({type: 'FOO'});
+
+    assert.isTrue(reducer.getCall(1).calledWith(0, {type: 'FOO'}));
+    assert.isTrue(reducer.getCall(2).calledWith(0, {type: 'BAR'}));
+    assert.isTrue(reducer.getCall(3).calledWith(0, {type: 'BAZ'}));
+  });
+
+  it('should throw an invariant when non function is provided', () => {
+    try {
+      sagaMiddleware(() => {}, 'foobar');
+      assert.isTrue(false);
+    } catch (ex) {
+      assert.equal(ex.message, 'Invariant violation: All the provided sagas must be typeof function');
+    }
+  });
+});

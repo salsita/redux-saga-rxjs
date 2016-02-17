60dbe2d04338f392bda483bb4229eeb1e632b731
Tomas Weiss <tomasw@salsitasoft.com>
2016-02-17 16:02:05 +0100

    code comments

--- examples/undo-redo-optimistic/src/sagas/commandSaga.js
+++ examples/undo-redo-optimistic/src/sagas/commandSaga.js
@@ -16,11 +16,24 @@ const unstableApi = info => {
 
 const actionPredicate = actions => ({ action }) => actions.some(someAction => someAction === action.type);
 
+// Filter out all the user initiated (non empty) ADD_TODO actions
+// and map them to provide client generated ID (the ID is used for optimistic updates)
+//
+// Handling ADD_TODO_ID in reducer will create transient TODO with clientId assigned.
+// This todo may eventually get deleted if the API fails
 const generateClientIdSaga = iterable => iterable
   .filter(actionPredicate([Actions.ADD_TODO]))
   .filter(({ action }) => action.payload !== '')
   .map(({ action }) => ActionCreators.addTodoId(globalClientId++, action.payload));
 
+// The main saga responsible for calling the API.
+// Whenever ADD_TODO_ID action kicks in, API call is executed.
+//
+// When API succeeds we need to provide `clientId` and `serverId` to reducer so that it's possible to assign the serverId to
+// transient todo record which now becomes persistent (commited). Also it's important to provide original action which
+// will act as "Command" which can be later used for redo.
+//
+// If the API fails, we'll provide just clientId so that the Todo can be rolled back
 const createTodoSaga = iterable => iterable
   .filter(actionPredicate([Actions.ADD_TODO_ID]))
   .flatMap(({ action }) => {
@@ -33,6 +46,8 @@ const createTodoSaga = iterable => iterable
         Observable.of(ActionCreators.addTodoFailed(clientId)));
   });
 
+// Whenever UNDO action kicks in
+// API call for undoing is executed and reducer is notified about success/failure by dispatching action
 const undoSaga = iterable => iterable
   .filter(actionPredicate([Actions.UNDO]))
   .flatMap(({ action }) =>
@@ -41,6 +56,9 @@ const undoSaga = iterable => iterable
         .catch(() =>
           Observable.of(ActionCreators.undoFailed(action.payload.serverId))));
 
+// This is a bit tricky
+// 1) Redo saga must map REDO action to original Command (which in our case is ADD_TODO_ID action)
+// 2) However, we need to wait for successful redoing of the command - the second branch of the merge
 const redoSaga = iterable => iterable
   .filter(actionPredicate([Actions.REDO]))
   .flatMap(({ action }) => Observable.merge(
@@ -51,6 +69,9 @@ const redoSaga = iterable => iterable
         .map(() => ActionCreators.redone())
     ));
 
+// Whenever we get ADD_TODO immediately followed by TODO_ADDED we
+// want to clear the redo log, because when user creates new todo, it doesn't
+// make sense to keep the previous redo log.
 const clearRedoLogSaga = iterable => iterable
   .filter(actionPredicate([Actions.ADD_TODO]))
   .flatMap(() => iterable
